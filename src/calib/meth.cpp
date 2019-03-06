63852520f9862c294259725d69859698d5abfc08
Ander <armath123@gmail.com>
2019-03-06 14:45:49 +1000

    Add LICENSE

--- src/calib/meth.cpp
+++ src/calib/meth.cpp
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2015-2017, 2019 Andrey Baranov <armath123@gmail.com>
+ *
+ * This file is part of CALIB (Contour Advection Library).
+ *
+ * CALIB is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * CALIB is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with CALIB;
+ * if not, see <http://www.gnu.org/licenses/>
+**/
+
 #include <calib/calib.hpp>
 
 #include <algorithm>
